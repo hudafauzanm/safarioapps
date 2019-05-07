b768a6d138567b366b110cf256fdbe6a50212a18
Unknown <hudafauzanm@gmail.com>
2019-05-07 23:36:30 +0700

    db

--- db_config.js
+++ db_config.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "safario"
+    host: "us-cdbr-iron-east-02.cleardb.net",
+    user: "b1af23b89da3a7",
+    password: "c5d4e8b0@us",
+    database: "heroku_327ce4da905106c"
 });
 
 db.connect(function(err) {
